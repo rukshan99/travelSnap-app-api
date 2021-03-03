19e4573bc89be268fc488b4c0154d453f1286584
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2021-03-03 15:04:17 +0530

    Implement update place and delete place operations

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -2,7 +2,7 @@ const uuid = require('uuid');
 
 const HttpError = require('../models/http-error');
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
     {
       id: 'p1',
       title: 'Empire State Building',
@@ -69,6 +69,30 @@ const createPlace = (req, res, next) => {
     res.status(201).json({place: createdPlace});
 };
 
+const updatePlaceById = (req, res, next) => {
+    const { title, description } = req.body;
+    const placeId = req.params.pid;
+
+    const updatedPlace = {... DUMMY_PLACES.find(p => p.id === placeId)};
+    const placeInex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+    updatedPlace.title = title;
+    updatedPlace.description = description;
+
+    DUMMY_PLACES[placeInex] = updatedPlace;
+
+    res.status(200).json({place: updatedPlace});
+};
+
+const deletePlaceById = (req, res, next) => {
+    const placeId = req.params.pid;
+
+    DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
+
+    res.status(200).json({message: 'Place deleted successfully.'});
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createPlace = createPlace;
+exports.updatePlaceById = updatePlaceById;
+exports.deletePlaceById = deletePlaceById;
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -7,6 +7,10 @@ router.get('/:pid', placesControllers.getPlaceById);
 
 router.get('/user/:uid', placesControllers.getPlaceByUserId);
 
-router.post('/', placesControllers.createPlace);
+router.post('/:pid', placesControllers.createPlace);
+
+router.patch('/:pid', placesControllers.updatePlaceById);
+
+router.delete('/:pid', placesControllers.deletePlaceById);
 
 module.exports = router;
